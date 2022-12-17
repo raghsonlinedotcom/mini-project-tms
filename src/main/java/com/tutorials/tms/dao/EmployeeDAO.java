e3085a53059088bb34b3323066b3e21b6ed97ba0
Raghavan alias Saravanan Muthu <raghavan.muthu@outlook.com>
2022-12-17 18:51:05 +0530

    Revert "TMS23 - ConfigFile changes added"

--- src/main/java/com/tutorials/tms/dao/EmployeeDAO.java
+++ src/main/java/com/tutorials/tms/dao/EmployeeDAO.java
@@ -5,6 +5,4 @@ import com.tutorials.tms.bo.Employee;
 public interface EmployeeDAO {
 	
 	public abstract int createEmployee(Employee employee) throws Exception;
-	
-	public abstract int getCount() throws Exception;
 }
