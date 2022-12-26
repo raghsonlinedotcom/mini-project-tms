56eb1f7ca8c114bf6f73f725071227293d822194
Karthik Kiran <karthikhkiran8@gmail.com>
2022-12-26 23:02:41 +0530

    added Login Junit test

---
+++ src/test/java/com/tutorials/tms/junit/LogInJUnitTestTest.java
@@ -0,0 +1,14 @@
+package com.tutorials.tms.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LogInJUnitTestTest {
+
+	@Test
+	void loginTest() {
+		fail("Not yet implemented");
+	}
+
+}
