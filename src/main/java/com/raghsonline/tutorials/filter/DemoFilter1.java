6cfb05c7d61e97df8dd7e9b2918084f59006b31c
Raghavan alias Saravanan Muthu <raghavan.muthu@outlook.com>
2023-01-03 20:11:07 +0530

    SQL File Issue fixed in DDL Stmt, Screenshot for Filters added, DemoFilters added

---
+++ src/main/java/com/raghsonline/tutorials/filter/DemoFilter1.java
@@ -0,0 +1,56 @@
+package com.raghsonline.tutorials.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * Servlet Filter implementation class DemoFilter1
+ */
+@WebFilter(
+		urlPatterns = { "/*" }, 
+		initParams = { 
+				@WebInitParam(name = "admin-email", value = "admin@example.com", description = "Email of the Admin")
+		}
+	)
+public class DemoFilter1 implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public DemoFilter1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
